2308748ab9a88d68d162313debf5671828a9757e
jonibi8787 <jonibi8787@gmail.com>
2023-01-31 09:16:57 -0800

    api_advanced

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,32 +1,19 @@
 #!/usr/bin/python3
-'''A module containing functions for working with the Reddit API.
-'''
+'''queries and returns corresponding result from the Reddit API'''
 import requests
 
 
-BASE_URL = 'https://www.reddit.com'
-'''Reddit's base API URL.
-'''
-
-
 def number_of_subscribers(subreddit):
-    '''Retrieves the number of subscribers in a given subreddit.
     '''
-    api_headers = {
-        'Accept': 'application/json',
-        'User-Agent': ' '.join([
-            'Mozilla/5.0 (Windows NT 10.0; Win64; x64)',
-            'AppleWebKit/537.36 (KHTML, like Gecko)',
-            'Chrome/97.0.4692.71',
-            'Safari/537.36',
-            'Edg/97.0.1072.62'
-        ])
-    }
-    res = requests.get(
-        '{}/r/{}/about/.json'.format(BASE_URL, subreddit),
-        headers=api_headers,
-        allow_redirects=False
-    )
-    if res.status_code == 200:
-        return res.json()['data']['subscribers']
-    return 0
+    Description:
+    - queries and returns the number of subscibers
+      (not active users, total subscribers) for a given subreddit.
+    - returns 0 if an invalid subreddit is given
+    '''
+    endpoint = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    headers = {'User-Agent': 'My User Agent 1.0'}
+    res = requests.get(endpoint, headers=headers)
+
+    if res.status_code != 200:
+        return 0
+    return res.json()['data']['subscribers']
