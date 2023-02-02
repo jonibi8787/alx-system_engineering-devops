69b37dbcfd3cab4406164ea104990c8aa27c13cf
jonibi8787 <jonibi8787@gmail.com>
2023-02-01 18:59:50 -0800

    web stack debugging

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,4 +1,4 @@
-# debug faulty wordpress site
+# Fixes a faulty wordpress site
 exec { 'fix-wordpress':
   command => 'bash -c "sed -i s/class-wp-locale.phpp/class-wp-locale.php/ \
 /var/www/html/wp-settings.php; service apache2 restart"',
